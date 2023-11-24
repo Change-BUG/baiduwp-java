1829f83d9b33ab7e90d0ad16d4fdef3e58dcac4b
Change-BUG <2028744995@qq.com>
2023-11-24 16:23:02 +0800

    first commit

---
+++ src/main/java/com/example/baiduwpjava/BaiduwpJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.baiduwpjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaiduwpJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaiduwpJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/baiduwpjava/BaiduwpJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.baiduwpjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaiduwpJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
